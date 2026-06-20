890ece74d7788853777b47b5c06111b649120f51
Chaitanya0210 <rayewarchaitanya@gmail.com>
2026-06-20 21:32:31 +0530

    Add comparison operations demonstration in 04_comparision.js

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,14 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);
+
+// different datA type
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null>0); // in this case converts  null to a number
+console.log(null==0);  //  in this case do not convert
+console.log(null>=0);  // in this case also converts to a  number
