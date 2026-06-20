6c448319050d396c2a0c541e2f53481181f2684f
Chaitanya0210 <rayewarchaitanya@gmail.com>
2026-06-20 20:40:27 +0530

    Add data types demonstration in 02_dataTypes.js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+let name = "Chaitanya";
+let age = 21;
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true/fasle
+// null => standalone value
+// undefined =>
+// symbol => unique
+// object 
+
+console.log(typeof undefined); //  undefined
+console.log(typeof null);  // object
