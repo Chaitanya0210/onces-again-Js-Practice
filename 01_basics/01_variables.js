47bfa6031fa5ab38c249f9557e3d2e72835f5bea
Chaitanya0210 <rayewarchaitanya@gmail.com>
2026-06-20 20:21:29 +0530

    Add variable declarations and remove unused app.js file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 12345
+let accountEmail = "rayewarchaitany@gamil.com"
+var accountPassword = "12345"
+accountCity = "Nanded"  //never use this method ,yes it assigns the variable automatically but a bad practice
+
+
+//see const is used to assign value permanantly , can't be modified further
+// let is used as normal variable which can be changed further as per need
+// (var was used earlier) :- had scope issues;  PREFER NOT TO USE "VAR" 
+
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
