1d4bec2859daf68e092a7dec81bc0c799cdc4025
Chaitanya0210 <rayewarchaitanya@gmail.com>
2026-06-21 21:57:29 +0530

    Refactor console log statements and improve function declaration examples in 02_scopes.js

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,8 +4,49 @@ if(true){
     var c = 30 // this gets printed , as it does not follows scope boundaries
 }
 
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
 
 // IMP POINT => global scope in (NODE.JS) environment ans in (CONSOLE) of browser is different
+
+
+function one(){
+    const username = "Chaitanya"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two();
+    
+}
+
+//  one()
+
+if(true){
+    const username =  "Chaitanya"
+    if(username==="Chaitanya"){
+        const website = " youtube"
+        console.log(username +  website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++++ intresting +++++++++++++++++++++
+console.log(addone(5));
+function addone(num){ //  if function is declared in such methoed it can be accessed before hand also
+    return ++num
+}
+
+// addtwo(5)  // this will through error
+
+const addtwo = function(num){ // in this type  of function declaration we have to call afterwards only 
+    return num+2
+}
+
+addtwo(5);
