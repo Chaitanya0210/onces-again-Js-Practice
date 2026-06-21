2323378c7a87b6dfa88f0f4264cac5323634860e
Chaitanya0210 <rayewarchaitanya@gmail.com>
2026-06-21 20:03:00 +0530

    Add course object demonstration and destructuring example in 04_objects.js; remove unused tempCodeRunnerFile.js

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -25,5 +25,17 @@ const obj2 = {3:"c", 4:"d"}
 
 // const obj3 = Object.assign(obj1,obj2)
 const obj3 ={...obj1,...obj2}
-console.log(obj3);
+console.log(obj3); 
+
+const course ={
+    courseName: "Js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+const {courseInstructor} = course
+
+console.log(courseInstructor);
+
+
 
